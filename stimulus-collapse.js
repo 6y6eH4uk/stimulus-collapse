f675af540b35e55358b898aa209194c67d3641da
6y6eH4uk <35969123+6y6eH4uk@users.noreply.github.com>
2021-05-31 22:06:04 +0300

    Update stimulus-collapse.js

--- stimulus-collapse.js
+++ stimulus-collapse.js
@@ -1,52 +1,111 @@
-class SliderController extends Stimulus.Controller {
-    static targets = ['slider', 'dot']
-
-    static values = {
-        slidesToShow: Number,
-        slidesToScroll: Number,
-        fade: Boolean,
-        index: Number
-    }
-
-    static classes = ['activeDot']
-
-    connect() {
-        $(this.sliderTarget).slick({
-            slidesToShow: this.slidesToShowValue,
-            slidesToScroll: this.slidesToScrollValue,
-            arrows: false,
-            fade: this.fadeValue
-        })
-
-        $(this.sliderTarget).on('beforeChange', (event, slick, currentSlide, nextSlide) => {
-            this.indexValue = nextSlide
-        })
-    }
-
-    indexValueChanged() {
-        if (this.hasActiveDotClass && this.hasDotTarget) {
-            this.dotTargets.forEach(el => {
-                if (el.dataset.slideIndex == this.indexValue) {
-                    el.classList.add(...this.activeDotClass.split(' '))
-                } else {
-                    this.activeDotClass.split(' ').forEach(className => {
-                        el.classList.remove(className)
-                    })
-                }
-            })
+/*
+	Событие stimulus-collapse-toggle:
+    
+    {
+    	detail: {
+        	status: true
         }
     }
-
-    prev() {
-        $(this.sliderTarget).slick('slickPrev')
+*/
+class CollapseController extends Stimulus.Controller {
+	static targets = ['content', 'wrapper']
+    
+    static values = {
+    	status: Boolean,
+        transition: Number
     }
 
-    next() {
-        $(this.sliderTarget).slick('slickNext')
-    }
+	initialize() {
+        if (!document.querySelector('#collapse-style')) {
+        	document.querySelector('head').insertAdjacentHTML('beforeend', `
+                <style id="collapse-style">
+                    .collapse {
+                        display: none;
+                    }
 
-    goTo({ currentTarget }) {
-        $(this.sliderTarget).slick('slickGoTo', currentTarget.dataset.slideIndex ?? 0)
+                    .collapse.show {
+                        display: block;
+                    }
+
+                    .collapsing {
+                        position: relative;
+                        height: 0;
+                        overflow: hidden;
+                        transition: height ${this.hasTransitionValue ? this.transitionValue : '0.35'}s ease;
+                    }
+                </style>
+            `)
+        }
+    }
+    
+    connect() {
+    	if (this.statusValue) {
+       		this.wrapperTarget.classList.add('collapse', 'show')
+        } else {
+        	this.wrapperTarget.classList.add('collapse')
+        }
+    }
+    
+    actualStatus() {
+    	return this.wrapperTarget.classList.contains('collapse') && this.wrapperTarget.classList.contains('show')
+    }
+    
+    statusValueChanged() {
+    	if (this.waitTransition) {
+        	return
+        }
+
+        if (this.statusValue === this.actualStatus()) {
+        	return
+        }
+        
+    	this.waitTransition = true
+        const status = this.statusValue
+        
+        this.element.dispatchEvent(new CustomEvent('stimulus-collapse-toggle', {
+        	detail: {
+            	status: status
+            }
+        }))
+        
+        if (!status) {
+        	this.wrapperTarget.style.height = this.contentTarget.scrollHeight+'px'
+        }
+        
+        this.wrapperTarget.classList.add('collapsing')
+        this.wrapperTarget.classList.remove('collapse', 'show')
+        
+        if (status) {
+            this.wrapperTarget.style.height = this.contentTarget.scrollHeight+'px'
+        } else {
+            setTimeout(() => {this.wrapperTarget.style.height = ''})
+        }
+        
+    	setTimeout(() => {
+        	this.wrapperTarget.classList.remove('collapsing')
+            this.wrapperTarget.classList.add('collapse')
+            this.wrapperTarget.style.height = ''
+            
+            if (status) {
+            	this.wrapperTarget.classList.add('show')
+            }
+            
+        	this.waitTransition = false
+        
+            setTimeout(() => {this.statusValueChanged()}, 100)
+        }, (this.hasTransitionValue ? this.transitionValue : 0.35)*1000)
+    }
+    
+    toggle() {
+    	this.statusValue = !this.statusValue
+    }
+    
+    on() {
+    	this.statusValue = true
+    }
+    
+    off() {
+    	this.statusValue = false
     }
 }
 
@@ -58,6 +117,6 @@ if (window.stimulusApplication) {
     application = Stimulus.Application.start()
 }
 
-application.register('slider', SliderController)
+application.register('collapse', CollapseController)
 
 window.stimulusApplication = application
